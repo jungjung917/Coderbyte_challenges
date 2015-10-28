8ce81968df0726755986eedf79aacfbded206af6
Zhengshi Lucia <zhengshizhao917@gmail.com>
2015-10-28 11:42:56 -0400

    solution

--- easy/solutions/PowerofTwo.py
+++ easy/solutions/PowerofTwo.py
@@ -8,9 +8,5 @@ def PowersofTwo(num):
             return "ture"
         else: 
 	    return PowersofTwo(num // 2)
-				    
 				        
-					# keep this function call here  
-					# to see how to enter arguments in Python scroll down
-					
 print PowersofTwo(128) 
