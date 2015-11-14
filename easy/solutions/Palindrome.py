8e7e9fbc13cf5f9d2f653633ebab4222870a2eb1
Zhengshi Lucia <zhengshizhao917@gmail.com>
2015-11-14 01:19:06 -0500

    solution

---
+++ easy/solutions/Palindrome.py
@@ -0,0 +1,13 @@
+"""
+Have the function Palindrome(str) take the str parameter being passed and return the string true if the parameter is a palindrome, (the string is the same forward as it is backward) otherwise return the string false. For example: "racecar" is also "racecar" backwards. Punctuation and numbers will not be part of the string. 
+"""
+def Palindrome(str):
+    str =''.join(str.split(" "))
+    str_reverse = ""
+    for letter in str:
+        str_reverse = letter + str_reverse
+    if str == str_reverse:
+        return "true"
+    else:
+        return "false"
+print Palindrome("racecar")
